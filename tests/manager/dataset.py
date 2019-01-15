f1eb231421070a0d5b676671b6bb7dd39aef3b42
Jeff Hilton <jeffhilton.code@gmail.com>
2019-01-14 23:28:28 -0700

    Testing dataset.

--- tests/manager/dataset.py
+++ tests/manager/dataset.py
@@ -31,3 +31,32 @@ class DatasetTest(TestCase):
 
         self.assertTrue(dataset.attribute_name(3) == "class")
         self.assertTrue(dataset.attribute_name(0) == 'x1')
+
+    def test_attributes(self):
+        dataset = Dataset(self.file)
+
+        datum = dataset.get(0)
+        self.assertTrue(datum[0] == 0.1)
+        self.assertTrue(datum[1] == 0.1)
+        self.assertTrue(datum[2] == 0)
+        self.assertTrue(datum[3] == 0)
+
+    def test_splits(self):
+        dataset = Dataset(self.file)
+        set1, set2 = dataset.split()
+        self.assertTrue(set1.size == 4)
+        self.assertTrue(set2.size == 4)
+
+        dataset = Dataset(self.file)
+        set1, set2, set3 = dataset.split([1/3, 1/3, 1/3])
+        self.assertTrue(set1.size == 2)
+        self.assertTrue(set2.size == 2)
+        self.assertTrue(set3.size == 4)
+
+    def test_norm(self):
+        #TODO test
+        pass
+
+    def test_shuffle(self):
+        #TODO test
+        pass
